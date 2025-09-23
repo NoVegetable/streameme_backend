0d75fd5b6ed50b57b12689bfdbf585f912d9bd93
NoVegetable <jinkey0202@gmail.com>
2025-09-23 14:56:30 +0800

    Add --port argumenet for custom port binding
    The default port is still 9090, however, user can now specify other port
    as long as it is not already in use.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_cors::Cors;
 use actix_multipart::form::tempfile::TempFileConfig;
 use actix_web::{App, HttpServer, middleware};
+use clap::{Arg, Command, value_parser};
 use env_logger::Env;
 use std::net::Ipv4Addr;
 use std::sync::{Arc, Mutex};
@@ -9,6 +10,19 @@ use tempfile::TempDir;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let matches = Command::new("streameme_backend")
+        .arg(
+            Arg::new("port")
+                .help("The port to listen on")
+                .short('p')
+                .long("port")
+                .value_parser(value_parser!(u16))
+                .default_value("9090"),
+        )
+        .get_matches();
+
+    let port = *matches.get_one::<u16>("port").unwrap();
+
     env_logger::init_from_env(Env::new().default_filter_or("info"));
 
     let tmp_dir = Arc::new(Mutex::new(TempDir::new_in(".")?));
@@ -29,10 +43,10 @@ async fn main() -> std::io::Result<()> {
             .app_data(TempFileConfig::default().directory(path))
             .configure(handlers::config)
     })
-    .bind((Ipv4Addr::UNSPECIFIED, 9090))?
+    .bind((Ipv4Addr::UNSPECIFIED, port))?
     .run();
 
-    log::info!("start HTTP server at http://localhost:9090");
+    log::info!("start HTTP server at http://localhost:{}", port);
 
     server.await
 }
